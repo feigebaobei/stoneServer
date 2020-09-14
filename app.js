e104ee466e4935f389cbab63bcc57137e1a550e3
feigebaobei <18515195415@163.com>
2020-09-14 14:56:33 +0800

    添加randomCode

--- app.js
+++ app.js
@@ -3,9 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var redis = require('redis')
+var config = require('./lib/config')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var randomCodeRouter = require('./routes/randomCode');
+
+// 连接数据库
+let red_config = config.redis,
+    RED_HOST = red_config.host,
+    RED_PWD = red_config.pass,
+    RED_PORT = red_config.port,
+    RED_OPTS = {auth_pass: RED_PWD},
+    client = redis.createClient(RED_PORT, RED_HOST, RED_OPTS)
+client.on('ready', (res) => {
+  console.log('ready')
+})
+client.on('end', (res) => {
+  console.log('end')
+})
+client.on('error', (error) => {
+  console.log('error', error)
+})
+client.on('connect', (res) => {
+  console.log('connect')
+})
 
 var app = express();
 
@@ -21,7 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/public', express.static('public'))
+app.use('/randomCode', randomCodeRouter);
+// app.use('/public', express.static('public'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/randomCode.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('tk6aeogin5gzo95q8h50k49tzu9h64mujqh3hczn8ka26lgiwj6vqpynj5otqbljgjhbkxwd2dtyky1v8drn15tuq3scav2thckz18y3hxqd121auu61acki7sci1vjm');
+});
+
+module.exports = router;
